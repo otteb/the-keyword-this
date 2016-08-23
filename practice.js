1c337d19df1ce1663dd44e810558db519bdf6997
Brian Otte <brianotte@Brians-MacBook-Pro.local>
2016-08-23 11:40:40 -0600

    done

--- practice.js
+++ practice.js
@@ -23,9 +23,26 @@
   //email --> which is a string
   //getUsername --> which is a function that returns the current object's username property. *Don't use 'user' instead use the 'this' keyword*
 
-    //Code Here
+var user = {
+  username: 'otteb',
+  email: 'briantron@gmail.com',
+  getUsername: function()
+  {
+    return this.username;
+  }
+
+}
 
 //Now, invoke the getUsername method and verify you got the username of the object and not anything else.
+
+
+
+
+
+
+
+
+
 
 
 //Next Problem
@@ -35,6 +52,19 @@
 
   //Function Invocations Here
 
+function Car(make, model, year)
+{
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.move = 0;
+  this.moveCar = function()
+  {
+    this.move += 10;
+    return this.move;
+  }
+}
+
 var prius = new Car('Toyota', 'Prius', 2011);
 var mustang = new Car('Ford', 'Mustang', 2013);
 
@@ -42,6 +72,7 @@ prius.moveCar(); //increments prius' move property by 10. Returns the new move p
 mustang.moveCar(); //increments mustang' move property by 10. Returns the new move property.
 
 //Hint, you'll need to add a move property and write a moveCar function which is added to every object that is being returned from the Car function. You'll also need to use the 'this' keyword properly in order to make sure you're invoking moveCar on the right object (prius vs mustang).
+
 
 
 
@@ -69,7 +100,7 @@ var getMyUsername = function() {
  return this.username;
 };
 
-var userName = getMyUsername(); //Fix this
+var userName = getMyUsername.bind(myUser)(); //Fix this
 
 //Above you're given an object, a function, and a setTimeout invocation. After 5 seconds, what will the getUsername function return?
 //Note(no tests)
